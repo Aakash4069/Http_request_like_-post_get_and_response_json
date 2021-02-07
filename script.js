20bf91986fe45b54be4eef8d540009fd74d540ee
aakash4069 <aakash4069@gmail.com>
2021-02-07 22:47:47 +0530

    printing only in console

--- script.js
+++ script.js
@@ -5,14 +5,30 @@ let form =document.getElementById('form')
 form.addEventListener('submit',function(e){
     e.preventDefault();//  to pravent from auto submition 
      
-    let name =document.getElementById('name');
-    let body =document.getElementById('body');
-    let id=document.getElementById('id');
+    let name =document.getElementById('name').value;
+    let body =document.getElementById('body').value;
+    // let id=document.getElementById('id').value;
     
     //fetch post request
-    fetch("https://jsonplaceholder.typicode.com/posts");
+    fetch("https://jsonplaceholder.typicode.com/posts",{
 
-
+        method:'Post',
+        body:JSON.stringify({
+            titile:name,
+            body:body,
+            id:id
+        }),
+        headers:{
+            "Content-Type":"application/json; charset=UTF-8"
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+        
 })
 
-module.export()
+// module.export()
